14c1ccd0da58242f7df12e3e2e93d36befde3297
ganand2021 <ganand@seas.upenn.edu>
2023-07-09 21:24:02 -0400

    URL edit

--- fastapi_test_classification.py
+++ fastapi_test_classification.py
@@ -1,9 +1,10 @@
 import requests
 
 URL_ENDPOINT = 'http://127.0.0.1:8000'
+# URL_ENDPOINT = 'https://simbiosys-390618.ue.r.appspot.com'
 classification_sample_path = './data/classification_dataset/test/CTCL/1_crop_3.png'
 
-response = requests.post("http://localhost:8000/classification", files = {'image': open(classification_sample_path, 'rb')})
+response = requests.post(URL_ENDPOINT+"/classification", files = {'image': open(classification_sample_path, 'rb')})
 
 result = response.json()
 print(result)
--- fastapi_test_combined.py
+++ fastapi_test_combined.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 URL_ENDPOINT = 'http://127.0.0.1:8000'
+# URL_ENDPOINT = 'https://simbiosys-390618.ue.r.appspot.com'
 combined_sample_path = './data/segmentation_dataset/image/p01-10005(54191.8632)600,100-crop_tif.tif'
 
-response = requests.post("http://localhost:8000/combined", files = {'image': open(combined_sample_path, 'rb')})
+response = requests.post(URL_ENDPOINT+"/combined", files = {'image': open(combined_sample_path, 'rb')})
 
 result = response.json()
 print(result)
